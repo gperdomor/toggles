1fb284f9d4d14fd173882a4b65caf028077adf04
Gustavo Perdomo <gperdomor@gmail.com>
2021-05-22 23:43:01 -0400

    feat(api): add initial security and compress integration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,17 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app/app.module';
+import { configure } from './configure';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
+
+  configure(app);
+
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
